32e0210177d12348311675b38f6c5193f8c70822
Adyaprana <120256356+Adyaprana@users.noreply.github.com>
2025-11-10 00:13:31 +0530

    Removed the <folder_name> directory

--- 1/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('menu-toggle');
-    const navLinks = document.getElementById('nav-links');
-
-    toggleButton.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
